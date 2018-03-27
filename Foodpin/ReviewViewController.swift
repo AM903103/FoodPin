bd57b4be1c399e45c2da07ac2cf95174739b7c6d
am903103 <am903103@au.edu.tw>
2018-03-27 15:36:03 +0800

    animation

--- Foodpin/ReviewViewController.swift
+++ Foodpin/ReviewViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ReviewViewController: UIViewController {
 
     @IBOutlet var backgroundImageView:UIImageView!
+    @IBOutlet var containerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        containerView.transform = CGAffineTransform.init(scaleX: 0,y: 0)
+
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
@@ -26,7 +30,12 @@ class ReviewViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 3, animations: {
+            self.containerView.transform = CGAffineTransform.identity
+        })
+    }
 
     /*
     // MARK: - Navigation
