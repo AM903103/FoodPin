a993ebc4eba6e27a02341d65cd2e98a60f59409e
am903103 <am903103@au.edu.tw>
2018-03-27 15:19:10 +0800

    blurEffectView

---
+++ Foodpin/ReviewViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ReviewViewController.swift
+//  Foodpin
+//
+//  Created by Apple on 2018/3/27.
+//  Copyright © 2018年 Gamma. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    @IBOutlet var backgroundImageView:UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
